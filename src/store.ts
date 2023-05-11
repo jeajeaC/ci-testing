af138322b638f389444f2b981f92b84c95e6dae1
rhy0u <jsn.centaure@gmail.com>
2023-05-11 17:32:28 +0200

    test

--- src/store.ts
+++ src/store.ts
@@ -12,7 +12,7 @@ export const store = configureStore({
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(beerApi.middleware),
 })
-
+// caca
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
