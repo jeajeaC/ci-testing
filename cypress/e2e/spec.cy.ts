1beae944c06f36c73b23468754e3e699591a3b88
rhy0u <jsn.centaure@gmail.com>
2023-05-12 14:58:09 +0200

    argos

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,13 +1,12 @@
 describe("cart", () => {
     it("should addand remove an item from cart", () => {
         cy.visit("http://localhost:5173")
-        cy.screenshot()
+        cy.argosScreenshot("home without cart")
         cy.contains("The Cart App")
         cy.get("ul > li:nth-child(1) > div > article > .add-button").click()
-        cy.screenshot()
+        cy.argosScreenshot("home with cart")
         cy.contains("item added to cart")
         cy.get(".Toastify__close-button ").click()
-        cy.screenshot()
 
         // cy.get("ul > li:nth-child(1) > div > .item-delete-button").click() // can't find how to swipe
     })
@@ -16,12 +15,10 @@ describe("cart", () => {
         cy.visit("http://localhost:5173")
         cy.contains("The Cart App")
         cy.get("ul > li:nth-child(1) > div > article > .add-button").click()
-        cy.screenshot()
         cy.contains("item added to cart")
         cy.get(".Toastify__close-button ").click()
-        cy.screenshot()
         cy.get("footer > button").click()
         cy.contains("removed everything from cart")
-        cy.screenshot()
+        cy.argosScreenshot("back to nothing in cart")
     })
 })
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,3 +1,5 @@
+import "@argos-ci/cypress/support"
+
 /// <reference types="cypress" />
 // ***********************************************
 // This example commands.ts shows you how to
